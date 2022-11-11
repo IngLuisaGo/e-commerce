968a6b1e18b7cffaf97ba8bac437f82e8b2d0f20
Luisa Gomez <ingluisago@gmail.com>
2022-11-10 22:11:21 -0500

    Se modifica modelo de compras quitando topes de los campos

--- proyecto_back/models/compras.model.js
+++ proyecto_back/models/compras.model.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ComprasSchema = new Schema({
-    id_cliente:{type:String, required:true, max:60},
-    subtotal:{type:Number, required:true, max:10000, min: 0},
-    id_productos:{type:[String], required:true, max:10}
+    id_cliente:{type:String, required:true},
+    subtotal:{type:Number, required:true},
+    id_productos:{type:[String], required:true}
 });
 
 module.exports = mongoose.model("compras", ComprasSchema);
